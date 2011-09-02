3e568c54225df42b36fad4f9d7b32e1d290ac13f
neektza <neektza@lappy.(none)>
2011-09-02 03:22:11 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SponsorshipManager::Application.config.secret_token = '8e33794bbc41207aa7dfcb503ee5684f34d8e0cc607d94e3cc1f93b73155d9af45efa63423bec36cc69c16674d08a837ed0c1391779f7621a1d409c8cc1fc2b5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+SponsorshipManager::Application.config.session_store :cookie_store, key: '_sponsorship_manager_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# SponsorshipManager::Application.config.session_store :active_record_store
